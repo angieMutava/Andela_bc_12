fa4133d62ebdc4c6c70da1d162cb6ba07575d5ca
angieMutava <mutavaangela@yahoo.com>
2016-11-11 05:42:28 -0800

    updated tests for prime generator

--- primeNumber/tests.py
+++ primeNumber/tests.py
@@ -1,14 +1,16 @@
 import unittest
-import primegenerator
+import primegenerator as testcases
 
 class prime_checker(unittest.TestCase):
 
-	def test_negative_numbers(self):
-		self.assertEqual(prime_generator(-5), "Negative numbers not allowed.")
+    def test_negative_numbers(self):
+        self.assertEqual(testcases.prime_generator(-5), "Negative numbers not allowed.")
 
-	def test_prime_generator(self):
-		self.assertEqual(prime_generator(10), [2, 3, 5, 7])
+    def test_prime_generator(self):
+        self.assertEqual(testcases.prime_generator(10), [2, 3, 5, 7])
 
-	def test_data_type(self):
-		self.assertEqual(prime_generator("str"), "Number should be integer")
+    def test_data_type(self):
+        self.assertEqual(testcases.prime_generator("str"), "Only integers allowed")
 
+    def test_number_one(self):
+    	self.assertEqual(testcases.prime_generator(1), [])
