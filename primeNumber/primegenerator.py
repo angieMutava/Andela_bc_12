a9e513a59ca338a3d4154c2db5da0217dee8a38b
angieMutava <mutavaangela@yahoo.com>
2016-11-16 08:12:52 -0800

    added chandes to prime generator

--- primeNumber/primegenerator.py
+++ primeNumber/primegenerator.py
@@ -1,15 +1,20 @@
+import math
 def is_prime(number):
+    """This is a function that checks whether the number passed is prime ."""
     if number <= 0:
         return False
+    elif number == 1:
+        return False    
     elif number == 2:
         return True
     else:
-        for i in range(2, number):
+        for i in range(2, int(math.sqrt(number))):
             if number % i == 0:
                 return False
         return True
 
 def prime_generator(number):
+    """It generates prime numbers in the given range of number"""
     if number < 0:
         return "Negative numbers not allowed."
     if type(number) != int:
